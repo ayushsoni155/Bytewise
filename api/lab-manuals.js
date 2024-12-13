2e38e1c1cbc64878e55122bae516d270a94992a4
Ayush soni <141997754+ayushsoni155@users.noreply.github.com>
2024-12-13 18:19:36 +0530

    Update lab-manuals.js

--- api/lab-manuals.js
+++ api/lab-manuals.js
@@ -23,7 +23,7 @@ function runMiddleware(req, res, fn) {
 
 // Setup the database connection pool
 const db = mysql.createPool({
-  host: process.env.DB_HOS,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
